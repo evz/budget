d18be6edeea898a6272f9e491cc4f879f3aa3e7a
Eric van Zanten <eric@e-vz.com>
2017-11-26 11:32:40 -0600

    Try more explicit decryption

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,4 +4,10 @@ set -euo pipefail
 # Make directory for project
 mkdir -p /home/evz/budget
 
-cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R evz.evz . && sudo -H -u evz blackbox_postdeploy
+DEPLOYMENT_DIR=/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
+chown -R evz.evz $DEPLOYMENT_DIR
+sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/app_config.py.gpg > $DEPLOYMENT_DIR/configs/app_config.py
+sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/nginx_template.conf.gpg > $DEPLOYMENT_DIR/configs/nginx_template.conf
+sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/supervisor_template.conf.gpg > $DEPLOYMENT_DIR/configs/supervisor_template.conf
+sudo -H -u evz gpg -d $DEPLOYMENT_DIR/scripts/after_install.sh.gpg > $DEPLOYMENT_DIR/scripts/after_install.sh
+sudo -H -u evz gpg -d $DEPLOYMENT_DIR/scripts/app_start.sh.gpg > $DEPLOYMENT_DIR/scripts/app_start.sh
