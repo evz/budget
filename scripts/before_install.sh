8345bc5c48b9fd5ab6a38348c4b6d99b0ead2aa4
Eric van Zanten <eric@e-vz.com>
2017-11-26 12:16:39 -0600

    Update deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,6 +9,6 @@ chown -R evz.evz $DEPLOYMENT_DIR
 sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/app_config.py.gpg > $DEPLOYMENT_DIR/configs/app_config.py
 sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/nginx_template.conf.gpg > $DEPLOYMENT_DIR/configs/nginx_template.conf
 sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/supervisor_template.conf.gpg > $DEPLOYMENT_DIR/configs/supervisor_template.conf
-sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/alembic.ini.gpg > $DEPLOYMENT_DIR/alembic.ini
+sudo -H -u evz gpg -d $DEPLOYMENT_DIR/configs/alembic_template.ini.gpg > $DEPLOYMENT_DIR/alembic.ini
 sudo -H -u evz gpg -d $DEPLOYMENT_DIR/scripts/after_install.sh.gpg > $DEPLOYMENT_DIR/scripts/after_install.sh
 sudo -H -u evz gpg -d $DEPLOYMENT_DIR/scripts/app_start.sh.gpg > $DEPLOYMENT_DIR/scripts/app_start.sh
